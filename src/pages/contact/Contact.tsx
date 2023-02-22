a1f06afee9ebb459721eb3ff29568c6f6a485d72
Elliot Robertson <elliotrobertson53@gmail.com>
2023-02-22 19:40:37 +0000

    feat(pages): added remaining pages

---
+++ src/pages/contact/Contact.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Header from '../../components/header/Header'
+
+const Portfolio: React.FC = () => {
+  return (
+    <div className='blog-container'>
+      <Header />
+    </div>
+  )
+}
+
+export default Portfolio
---
+++ src/pages/portfolio/Portfolio.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Header from '../../components/header/Header'
+
+const Contact: React.FC = () => {
+  return (
+    <div className='blog-container'>
+      <Header />
+    </div>
+  )
+}
+
+export default Contact
